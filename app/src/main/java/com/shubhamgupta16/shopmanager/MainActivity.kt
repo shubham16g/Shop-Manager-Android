7f89f99974ac918bacd4589d625f6e1ab22b3b93
Shubham Gupta <shubham.developer16@gmail.com>
2025-03-30 14:30:35 +0530

    Removed thermal printer code

--- app/src/main/java/com/shubhamgupta16/shopmanager/MainActivity.kt
+++ app/src/main/java/com/shubhamgupta16/shopmanager/MainActivity.kt
@@ -1,31 +1,19 @@
 package com.shubhamgupta16.shopmanager
 
-import android.Manifest
 import android.content.Intent
 import android.os.Bundle
-import android.util.Log
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.app.ActivityCompat
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 import androidx.core.view.updatePadding
-import com.dantsu.escposprinter.EscPosPrinter
 import com.shubhamgupta16.shopmanager.databinding.ActivityMainBinding
-import com.shubhamgupta16.shopmanager.invoice.ThermalPrinter
-import com.dantsu.escposprinter.connection.tcp.TcpConnection
 
 
 
 
 class MainActivity : AppCompatActivity() {
     private lateinit var binding: ActivityMainBinding
-
-    companion object {
-        private const val PERMISSION_BLUETOOTH = 12
-    }
-    private lateinit var thermalPrinter: ThermalPrinter
-    private var printer: EscPosPrinter?=null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -45,8 +33,6 @@ class MainActivity : AppCompatActivity() {
             insets
         }
 
-        thermalPrinter = ThermalPrinter(this)
-
         binding.products.setOnClickListener {
             startActivity(Intent(this, ProductsActivity::class.java))
         }
@@ -54,25 +40,5 @@ class MainActivity : AppCompatActivity() {
             startActivity(Intent(this, SellActivity::class.java))
         }
 
-        val pageData = ThermalPrinter.PageData(203, 48f, 32)
-        val tcpData  = ThermalPrinter.TCPData("192.168.1.3", 9300, 15)
-
-
-        binding.demoPrint.setOnClickListener {
-            Thread{
-                printer = thermalPrinter.getTCPPrinter(tcpData, pageData)
-                if (printer == null) {
-                    Log.d("TAG", "onCreate: no printer")
-                    ActivityCompat.requestPermissions(
-                        this, arrayOf(Manifest.permission.BLUETOOTH), PERMISSION_BLUETOOTH
-                    )
-                } else{
-                    Log.d("TAG", "onCreate: printing")
-                    printer!!.printFormattedText(thermalPrinter.getSamplePrintText())
-//                printing
-                }
-            }.start()
-
-        }
     }
 }
